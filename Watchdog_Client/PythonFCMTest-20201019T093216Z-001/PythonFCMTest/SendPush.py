753d3eb441576740d1d0f15915c73f4b0cb1fd16
amruta5694 <54052275+amruta5694@users.noreply.github.com>
2021-02-20 11:39:54 +1030

    Add files via upload

---
+++ Watchdog_Client/PythonFCMTest-20201019T093216Z-001/PythonFCMTest/SendPush.py
@@ -0,0 +1,9 @@
+from PythonFCMTest import FCMManager as fcm
+
+tokens = ["f74Jkk84SxyL7lvSwUsPGY:APA91bFTYXtMn9ffwM8rtba2TmlfEVD_592WdLAy_mw8hClANjQ27jENkZ4brBzRNtnSxASyRr8ZgK0zr4RLMBRZ26OTvHOnpC0L_EGYZWAcvhFXmyfOYwekj9vrdp-_sCDTLW9bZ22f"]
+#fIQFEqcXTo-pgttuHRjdKt:APA91bEbX7Gfank-D7szc1AbmLwBCazGERyxft5hZ06XtEaAZ2Qvqr_McVNyZGVkexP3A1hzDXie5rJ0deQ2LivpYM2Lp2U0iu7wTWxi6Pp1mKfg_rjdrq8P28Ri6fflARrX3SgCStYo
+#"dYV58gaxRxiJrEgPdG8a5i:APA91bF1kC1_OkoR0yLx4GaFiGbRWthIlo1tJhPVrNYcOlp-J_11VQIe3BhQukK4ctK0y_PmyCJQcDm64sVhGhy"
+#          "-6WUq11eitr8RbiRMi6677Koloxwzcomq9Mpx4Oq0A3T51GXusGOu"
+#"dYV58gaxRxiJrEgPdG8a5i:APA91bF1kC1_OkoR0yLx4GaFiGbRWthIlo1tJhPVrNYcOlp-J_11VQIe3BhQukK4ctK0y_PmyCJQcDm64sVhGhy"
+#         "-6WUq11eitr8RbiRMi6677Koloxwzcomq9Mpx4Oq0A3T51GXusGOu"
+fcm.sendPush("Hi", "This is my next msg", tokens)
